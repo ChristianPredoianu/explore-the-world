b712157da61dc35257f43a118da4b50a2ca4ed65
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-06 13:24:03 +0100

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import '@/sass/main.scss';
+
+library.add(fab, faFacebook);
 
 const router = createBrowserRouter([
   {
