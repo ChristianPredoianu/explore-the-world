d0d58bb5b871c9b7801f67c145d89446c82c5da7
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-07 13:37:55 +0100

    rendering through props

--- src/components/swiper/SlidesAutoSwiper.tsx
+++ src/components/swiper/SlidesAutoSwiper.tsx
@@ -1,64 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
-import LoadingSpinner from '@/components//ui/LoadingSpinner';
-
+import { ICountriesPhotos } from '@/types/apiTypes.interface';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import '@/components/swiper/SlidesAutoSwiper.scss';
 
-interface ICountries {
-  photos: [{ id: number; alt: string; src: { portrait: string } }];
+interface SwiperDataProps {
+  swiperData: ICountriesPhotos[];
+  mostPopularCountries: string[];
 }
 
-const mostPopularCountries = [
-  'england',
-  'italy',
-  'spain',
-  'japan',
-  'greece',
-  'argentina',
-];
-
-export default function SlidesAutoSwiper() {
-  const [countries, setCountries] = useState<ICountries[] | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+export default function SlidesAutoSwiper({
+  swiperData,
+  mostPopularCountries,
+}: SwiperDataProps) {
   const [swiperActiveIndex, setSwiperActiveIndex] = useState(1);
 
-  function fetchMostPopularCountries() {
-    const baseUrl = 'https://api.pexels.com/v1/search?query=';
-
-    setIsLoading(true);
-
-    const countriesPromises = mostPopularCountries.map((country) =>
-      fetch(`${baseUrl}${country}&per_page=2`, {
-        headers: {
-          Authorization: import.meta.env.VITE_PEXELS_API_KEY,
-        },
-      }).then((res) => res.json())
-    );
-
-    Promise.all(countriesPromises).then((data: ICountries[]) => {
-      setCountries(data);
-      setIsLoading(false);
-    });
-  }
-
-  let mostPopularCountriesSlides: JSX.Element | React.ReactElement[];
-
-  if (isLoading) {
-    mostPopularCountriesSlides = <LoadingSpinner />;
-  } else if (countries) {
-    mostPopularCountriesSlides = countries.map((country) => (
-      <SwiperSlide key={country.photos[0].id}>
-        <img src={country.photos[0].src.portrait} alt={country.photos[0].alt} />
-      </SwiperSlide>
-    ));
-  }
-
-  useEffect(() => {
-    fetchMostPopularCountries();
-  }, [mostPopularCountries]);
+  const mostPopularCountriesSlides = swiperData.map((country, index) => (
+    <SwiperSlide key={country.photos[0].id}>
+      <img src={country.photos[0].src.portrait} alt={country.photos[0].alt} />
+      <p className='slider-country'>{mostPopularCountries[index]}</p>
+    </SwiperSlide>
+  ));
 
   return (
     <>
@@ -67,16 +31,23 @@ export default function SlidesAutoSwiper() {
         spaceBetween={30}
         navigation={true}
         modules={[Navigation]}
-        onActiveIndexChange={(swiper) => {
-          setSwiperActiveIndex(swiper.activeIndex + 1);
-          console.log(swiper.activeIndex);
-        }}
+        onActiveIndexChange={(swiper) =>
+          setSwiperActiveIndex(swiper.activeIndex + 1)
+        }
         className='mySwiper'
       >
         {mostPopularCountriesSlides!}
         <div className='line'></div>
         <p className='counter'>{`0${swiperActiveIndex}`}</p>
       </Swiper>
+      <a
+        href='https://www.pexels.com'
+        target='_blank'
+        rel='noopener nofollow'
+        className='pexels'
+      >
+        Photos provided by Pexels
+      </a>
     </>
   );
 }
