fde7e1d5c3cb7606259cf766afc21659c82512b9
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-07 10:19:49 +0100

    typed mostpopularcountriesslides

--- src/components/swiper/SlidesAutoSwiper.tsx
+++ src/components/swiper/SlidesAutoSwiper.tsx
@@ -8,7 +8,7 @@ import 'swiper/css/navigation';
 import '@/components/swiper/SlidesAutoSwiper.scss';
 
 interface ICountries {
-  photos: [{ id: number; alt: string; src: { medium: string } }];
+  photos: [{ id: number; alt: string; src: { portrait: string } }];
 }
 
 const mostPopularCountries = [
@@ -23,6 +23,7 @@ const mostPopularCountries = [
 export default function SlidesAutoSwiper() {
   const [countries, setCountries] = useState<ICountries[] | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [swiperActiveIndex, setSwiperActiveIndex] = useState(1);
 
   function fetchMostPopularCountries() {
     const baseUrl = 'https://api.pexels.com/v1/search?query=';
@@ -43,21 +44,20 @@ export default function SlidesAutoSwiper() {
     });
   }
 
-  let mostPopularCountriesSlides;
+  let mostPopularCountriesSlides: JSX.Element | React.ReactElement[];
 
   if (isLoading) {
     mostPopularCountriesSlides = <LoadingSpinner />;
   } else if (countries) {
     mostPopularCountriesSlides = countries.map((country) => (
       <SwiperSlide key={country.photos[0].id}>
-        <img src={country.photos[0].src.medium} alt={country.photos[0].alt} />
+        <img src={country.photos[0].src.portrait} alt={country.photos[0].alt} />
       </SwiperSlide>
     ));
   }
 
   useEffect(() => {
     fetchMostPopularCountries();
-    console.log(countries);
   }, [mostPopularCountries]);
 
   return (
@@ -67,9 +67,15 @@ export default function SlidesAutoSwiper() {
         spaceBetween={30}
         navigation={true}
         modules={[Navigation]}
+        onActiveIndexChange={(swiper) => {
+          setSwiperActiveIndex(swiper.activeIndex + 1);
+          console.log(swiper.activeIndex);
+        }}
         className='mySwiper'
       >
-        {mostPopularCountriesSlides}
+        {mostPopularCountriesSlides!}
+        <div className='line'></div>
+        <p className='counter'>{`0${swiperActiveIndex}`}</p>
       </Swiper>
     </>
   );
