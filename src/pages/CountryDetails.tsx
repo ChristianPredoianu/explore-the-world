2c7c3f07c3989298da0c2ced40501b4db9a6bed6
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-13 19:10:17 +0100

    fixed fetchCountryDetails loader function with promise all to fetch details & images

--- src/pages/CountryDetails.tsx
+++ src/pages/CountryDetails.tsx
@@ -5,36 +5,42 @@ import classNames from 'classnames';
 import classes from '@/pages/CountryDetails.module.scss';
 
 export default function CountryDetails() {
-  const data = useLoaderData();
+  const countryDetailsData = useLoaderData();
 
-  console.log(data);
+  const [countryDetails, countryImages] = countryDetailsData.data;
+
+  console.log(countryImages);
 
   return (
     <>
       <CountryDetailsNav
-        flag={data[0].flags.png}
-        altSpelling={data[0].altSpellings[0]}
+        flag={countryDetails[0].flags.png}
+        altSpelling={countryDetails[0].altSpellings[0]}
       />
       <main className={classNames('container', classes.main)}>
         <section className={classNames(classes.sectionCountryInfo)}>
           <div className={classes.countryWrapper}>
-            <h1 className={classes.countryName}>{data[0].name.common}</h1>
+            <h1 className={classes.countryName}>
+              {countryDetails[0].name.common}
+            </h1>
             <img
-              src={data[0].flags.png}
+              src={countryDetails[0].flags.png}
               alt='country flag'
               className={classes.countryFlag}
             />
           </div>
           <div className={classes.countryInfo}>
-            <h2 className={classes.countryCapital}>{data[0].capital[0]}</h2>
+            <h2 className={classes.countryCapital}>
+              {countryDetails[0].capital[0]}
+            </h2>
             <div className={classes.countryInfoExplore}>
               <h3 className={classes.countryText}>
-                {`${data[0].name.common} you don't know, atypical, unexplored, unique..`}
+                {`${countryDetails[0].name.common} you don't know, atypical, unexplored, unique..`}
               </h3>
               <div className={classes.contryExploreWrapper}>
                 <h4 className={classes.countryExplore}>Explore</h4>
                 <img
-                  src={data[0].flags.png}
+                  src={countryDetails[0].flags.png}
                   alt='country flag'
                   className={classes.countryExploreFlag}
                 />
@@ -42,19 +48,27 @@ export default function CountryDetails() {
             </div>
           </div>
         </section>
-        <section className={classNames(classes.sectionPhotos)}>
-          {/* <h1>photos</h1> */}
-        </section>
+        <section className={classNames(classes.sectionPhotos)}></section>
       </main>
     </>
   );
 }
 
 export async function fetchCountryDetails({ params }: LoaderFunctionArgs) {
-  let url = `https://restcountries.com/v3.1/name/${params.countryId}`;
+  let countryDetailsUrl = `https://restcountries.com/v3.1/name/${params.countryId}`;
+  const countryImagesUrl = `https://api.pexels.com/v1/search?query=${params.countryId}&per_page=1`;
 
   if (params.countryId === 'england')
-    url = `https://restcountries.com/v3.1/name/gb`;
+    countryDetailsUrl = `https://restcountries.com/v3.1/name/gb`;
 
-  return await getApiData(url);
+  const countryDetailsPromise = getApiData(countryDetailsUrl);
+  const countryImagesPromise = getApiData(countryImagesUrl, {
+    headers: {
+      Authorization: import.meta.env.VITE_PEXELS_API_KEY,
+    },
+  });
+
+  const data = await Promise.all([countryDetailsPromise, countryImagesPromise]);
+
+  return { data };
 }
