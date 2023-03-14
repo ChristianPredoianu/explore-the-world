89bcc0ca3d5e4db69bc4d8a392b469aa78981355
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-14 15:16:53 +0100

    final

--- src/components/swiper/EffectCreativeSlider.tsx
+++ src/components/swiper/EffectCreativeSlider.tsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCreative } from 'swiper';
 import { ICountriesImages } from '@/types/apiTypes.interface';
+import { Autoplay } from 'swiper';
 
 import 'swiper/css';
 import 'swiper/css/effect-creative';
@@ -13,14 +14,26 @@ interface EffectCreativeSliderProps {
 export default function EffectCreativeSlider({
   images,
 }: EffectCreativeSliderProps) {
-  images.photos.splice(0, 1);
+  images.photos.shift();
+
+  const imgSlides = images.photos.map((image) => (
+    <SwiperSlide
+      key={image.id}
+      style={{
+        backgroundImage: `url('${image.src.large2x}')`,
+      }}
+    ></SwiperSlide>
+  ));
 
   return (
     <>
       <Swiper
         grabCursor={true}
         effect={'creative'}
-        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         creativeEffect={{
           prev: {
             shadow: true,
@@ -30,26 +43,10 @@ export default function EffectCreativeSlider({
             translate: ['100%', 0, 0],
           },
         }}
-        modules={[EffectCreative]}
+        modules={[EffectCreative, Autoplay]}
         className='mySwiper2'
       >
-        <SwiperSlide
-          style={{
-            backgroundImage: `url('${images.photos[0].src.large2x}')`,
-          }}
-        ></SwiperSlide>
-        <SwiperSlide
-          style={{
-            backgroundImage: `url('${images.photos[1].src.large2x}')`,
-          }}
-        ></SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {imgSlides}
       </Swiper>
     </>
   );
