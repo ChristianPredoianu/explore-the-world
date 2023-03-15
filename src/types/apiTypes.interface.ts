962b75791ce4e9c43227e434329bbe37487f9e5b
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-15 13:38:23 +0100

    added interface

--- src/types/apiTypes.interface.ts
+++ src/types/apiTypes.interface.ts
@@ -18,7 +18,12 @@ export interface ICountryDetails {
   };
   altSpellings: string;
   name: { common: string };
+  latlng: {
+    0: number;
+    1: number;
+  };
   capital: string;
+
   translations: {
     deu: {
       common: string;
@@ -43,4 +48,27 @@ export interface ICountryDetails {
 
 export interface ICountryDetailsData {
   data: [ICountryDetails[], ICountriesImages];
+}
+
+export interface IOpenWeatherToday {
+  main: {
+    feels_like: number;
+    temp: number;
+  };
+  sys: {
+    sunrise: number;
+    sunset: number;
+  };
+  weather: {
+    0: {
+      main: string;
+      description: string;
+      icon: string;
+    };
+  };
+  wind: {
+    deg: number;
+    speed: number;
+    gust: number;
+  };
 }
