005ed1b42eb8904c20d1c656caa5911884d5b175
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-15 13:39:48 +0100

    work

--- src/components/weather/TodaysWeather.tsx
+++ src/components/weather/TodaysWeather.tsx
@@ -1,3 +1,48 @@
-export default function TodaysWeather() {
-  return <p>todaysWeather</p>;
+import { IOpenWeatherToday } from '@/types/apiTypes.interface';
+
+import Card from '@/components/cards/TodaysWeatherCard';
+import classes from '@/components/weather/TodaysWeather.module.scss';
+
+interface TodaysWeatherProps {
+  data: IOpenWeatherToday;
+}
+
+function toTimeString(totalSeconds: number) {
+  const totalMs = totalSeconds * 1000;
+  const result = new Date(totalMs).toISOString().slice(11, 19);
+
+  return result;
+}
+
+export default function TodaysWeather({ data }: TodaysWeatherProps) {
+  return (
+    <div className={classes.cards}>
+      <Card
+        cardHeading='Weather'
+        descriptionOne='Temperature'
+        valueOne={`${data.main.temp.toFixed()} \xBAC`}
+        cardIcon={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        descriptionTwo='Feels like'
+        valueTwo={`${data.main.feels_like.toFixed()} \xBAC`}
+      >
+        <p className={classes.cardValue}>{data.weather[0].main}</p>
+      </Card>
+      <Card
+        cardHeading='Sunrise - Sunset'
+        descriptionOne='Sunrise'
+        valueOne={toTimeString(data.sys.sunrise)}
+        cardIcon='https://openweathermap.org/img/wn/01d@2x.png'
+        descriptionTwo='Sunset'
+        valueTwo={toTimeString(data.sys.sunset)}
+      />
+      <Card
+        cardHeading='Wind'
+        descriptionOne='Wind speed'
+        valueOne={`${data.wind.speed.toFixed()} m/s`}
+        cardIcon='https://openweathermap.org/img/wn/50d@2x.png'
+        descriptionTwo='gust'
+        valueTwo={data.wind.gust.toFixed()}
+      />
+    </div>
+  );
 }
