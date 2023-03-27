5fb53f4de5e74d9e7035191b251d7b823100a2aa
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-27 13:46:48 +0200

    made variables for jsx return

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,34 +19,36 @@ const mostPopularCountries = [
 export default function Home() {
   const countriesData = useLoaderData() as ICountriesImages[];
 
+  const heroContent = (
+    <div className={classNames('container', classes.heroContent)}>
+      <section className={classNames(classes.heroSectionHeadings)}>
+        <p className={classes.heroParagraph}>Travel website</p>
+        <h1 className={classes.heroPrimaryHeading}>Never stop exploring the world.</h1>
+        <h2 className={classes.heroSecondaryHeading}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type specimen
+          book.
+        </h2>
+        <div className={classes.inputWrapper}>
+          <SearchCountryInput />
+        </div>
+      </section>
+      <section className={classes.sectionSwiper}>
+        <SlidesAutoSwiper
+          swiperData={countriesData}
+          mostPopularCountries={mostPopularCountries}
+        />
+      </section>
+    </div>
+  );
+
   return (
     <>
       <MainNav />
       <main className={classes.main}>
         <div className={classes.overlay}></div>
-        <div className={classNames('container', classes.heroContent)}>
-          <section className={classNames(classes.heroSectionHeadings)}>
-            <p className={classes.heroParagraph}>Travel website</p>
-            <h1 className={classes.heroPrimaryHeading}>
-              Never stop exploring the world.
-            </h1>
-            <h2 className={classes.heroSecondaryHeading}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled it to
-              make a type specimen book.
-            </h2>
-            <div className={classes.inputWrapper}>
-              <SearchCountryInput />
-            </div>
-          </section>
-          <section className={classes.sectionSwiper}>
-            <SlidesAutoSwiper
-              swiperData={countriesData}
-              mostPopularCountries={mostPopularCountries}
-            />
-          </section>
-        </div>
+        {heroContent}
       </main>
     </>
   );
