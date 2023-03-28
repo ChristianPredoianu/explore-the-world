aa595dfa9a2f71cf874fae71f9384b2032fc1972
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-28 11:26:23 +0200

    added map

---
+++ src/components/map/CountryMap.tsx
@@ -0,0 +1,24 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+import '@/components/map/CountryMap.scss';
+
+interface CountryMapProps {
+  coords: number[];
+}
+
+export default function CountryMap({ coords }: CountryMapProps) {
+  const position: LatLngExpression = [coords[0], coords[1]]; // [latitude, longitude]
+  const zoomLevel = 7;
+
+  return (
+    <div id='map'>
+      <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}></Marker>
+      </MapContainer>
+    </div>
+  );
+}
