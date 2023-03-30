63881f08fec02165b180cf5d0f03b19bed38e0c9
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-30 15:36:54 +0200

    final

--- src/components/calendar/ReactCalendar.tsx
+++ src/components/calendar/ReactCalendar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import Calendar from 'react-calendar';
+import HolidaysList from '@/components/calendar/holidays/HolidaysList';
 import { baseCalendarificUrl } from '@/utils/urls';
 import { IHolidaysData } from '@/types/apiTypes.interface';
 import '@/components/calendar/ReactCalendar.scss';
@@ -14,22 +15,54 @@ export default function ReactCalendar({ countryCode }: CalendarProps) {
 
   const calendarificUrl = `${baseCalendarificUrl}&api_key=${
     import.meta.env.VITE_CALENDARIFIC_API_KEY
-  }&country=${countryCode}&year=${holidayYear}`;
+  }&country=${countryCode}&year=${holidayYear}&type=national`;
 
-  const [value, setValue] = useState(new Date());
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [holidayDescription, setHolidayDescription] = useState('');
 
   const [holidaysData] = useFetch<IHolidaysData>(calendarificUrl);
 
   function onChange(date: Date | Date[]) {
     if (date instanceof Date) {
-      setValue(date);
-      console.log(date);
+      setDate(date);
     }
+  }
+
+  function handleHolidayItemClick(date: string, description: string) {
+    setCalendarDate(date);
+    setHolidayDescription(description);
+  }
+
+  function setCalendarDate(date: string) {
+    setDate(new Date(date));
+  }
+  console.log(holidaysData);
+
+  function onActiveStartDateChange({ activeStartDate }: any) {
+    setCalendarDate(activeStartDate);
   }
 
   return (
     <>
-      <Calendar onChange={onChange} value={value} />
+      <div className='holidays-wrapper'>
+        <div className='calendar-wrapper'>
+          <Calendar
+            value={date}
+            activeStartDate={date}
+            onActiveStartDateChange={onActiveStartDateChange}
+            onChange={onChange}
+          />
+        </div>
+        <div className='holidays-list-wrapper'>
+          {holidaysData && (
+            <HolidaysList
+              holidaysData={holidaysData.response}
+              handleHolidayItemClick={handleHolidayItemClick}
+            />
+          )}
+        </div>
+      </div>
+      <p className='calendar-description'>{holidayDescription}</p>
     </>
   );
 }
