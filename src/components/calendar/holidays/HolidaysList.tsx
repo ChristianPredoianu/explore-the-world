8131f6884cfe63a9f4ddd05c4acc0988f623aa4a
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-30 15:36:21 +0200

    fixed list

---
+++ src/components/calendar/holidays/HolidaysList.tsx
@@ -0,0 +1,42 @@
+import HolidaysListItem from '@/components/calendar/holidays/HolidaysListItem';
+import classses from '@/components/calendar/holidays/HolidaysList.module.scss';
+
+interface HolidaysListProps {
+  holidaysData: {
+    holidays: {
+      name: string;
+      description: string;
+      id: number;
+      date: {
+        iso: string;
+      };
+    }[];
+  };
+
+  handleHolidayItemClick: (date: string, description: string) => void;
+}
+
+export default function HolidaysList({
+  holidaysData,
+  handleHolidayItemClick,
+}: HolidaysListProps) {
+  function addIdToHolidaysData() {
+    const holidaysDataWithIds = holidaysData.holidays.map((item, i) => {
+      return {
+        ...item,
+        id: i + 1,
+      };
+    });
+
+    return holidaysDataWithIds;
+  }
+
+  const holidaysListItems = addIdToHolidaysData().map((holiday) => (
+    <HolidaysListItem
+      key={holiday.id}
+      holiday={holiday}
+      handleHolidayItemClick={handleHolidayItemClick}
+    />
+  ));
+  return <ul className={classses.holidaysList}>{holidaysListItems}</ul>;
+}
