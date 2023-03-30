f355dc82c9cd03ef7adc86abacac64c5cad704fa
Christian Predoianu <christianpredoianu@yahoo.com>
2023-03-30 15:35:27 +0200

    fixed list item

--- src/components/calendar/holidays/HolidaysListItem.tsx
+++ src/components/calendar/holidays/HolidaysListItem.tsx
@@ -1,3 +1,27 @@
-export default function HolidaysListItem() {
-  return;
+import classes from '@/components/calendar/holidays/HolidaysListItem.module.scss';
+
+interface HolidaysListItemProps {
+  holiday: {
+    name: string;
+    description: string;
+    date: {
+      iso: string;
+    };
+  };
+
+  handleHolidayItemClick: (date: string, desctription: string) => void;
+}
+
+export default function HolidaysListItem({
+  holiday,
+  handleHolidayItemClick,
+}: HolidaysListItemProps) {
+  return (
+    <li
+      className={classes.holidayListItem}
+      onClick={() => handleHolidayItemClick(holiday.date.iso, holiday.description)}
+    >
+      {holiday.name}
+    </li>
+  );
 }
