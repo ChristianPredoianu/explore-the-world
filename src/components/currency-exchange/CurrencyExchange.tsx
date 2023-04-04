50942ccf32b4bedbcceb35e52dcae344f1625a45
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-04 16:40:46 +0200

    work

--- src/components/currency-exchange/CurrencyExchange.tsx
+++ src/components/currency-exchange/CurrencyExchange.tsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useFetch } from '@/hooks/useFetch';
 import classes from '@/components/currency-exchange/CurrencyExchange.module.scss';
 
-export default function CurrencyExchange() {
-  return <h1>currency</h1>;
+interface CurrencyExchangeProps {
+  currency: string;
+  countryCode: string;
+}
+
+export default function CurrencyExchange({
+  currency,
+  countryCode,
+}: CurrencyExchangeProps) {
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+
+  const countryFlag = `https://flagsapi.com/${countryCode}/flat/16.png`;
+
+  function handleChange(e) {
+    console.log(e.target.value);
+    setSelectedCurrency(e.target.value);
+  }
+
+  return (
+    <>
+      <article className={classes.currencyCard}>
+        <div className={classes.currencyFrom}>
+          <div className={classes.inputWrapper}>
+            <label htmlFor='currency from'>Amount</label>
+            <input type='text' className={classes.currencyInput} />
+          </div>
+          <div className={classes.currencyWrapper}>
+            <p>Swedish Crown</p>
+            <div className={classes.countryCurrency}>
+              <img src={countryFlag}></img>
+              <p>SEK</p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.currencyTo}>
+          <div className={classes.inputWrapper}>
+            <label htmlFor='currency to'>You Get</label>
+            <input type='text' className={classes.currencyInput} />
+          </div>
+          <div className={classes.selectWrapper}>
+            <label htmlFor='currency'>Euro</label>
+            <select
+              value={selectedCurrency}
+              onChange={handleChange}
+              className={classes.select}
+            >
+              <option value='apple'>Apple</option>
+              <option value='banana'>Banana</option>
+              <option value='orange'>Orange</option>
+            </select>
+          </div>
+        </div>
+      </article>
+      {/*  <img src={countryFlag}></img> */}
+    </>
+  );
 }
