f44bb14f4ef62c8f3513d218e0dfaca547331f69
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-11 16:22:14 +0200

    work on currency exchange

--- src/components/currency-exchange/CurrencyExchange.tsx
+++ src/components/currency-exchange/CurrencyExchange.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { baseExchangeRateUrl } from '@/utils/urls';
+import { currencyCountryCodes } from '@/utils/currencies';
 import { IExchangeRates } from '@/types/apiTypes.interface';
+
 import classes from '@/components/currency-exchange/CurrencyExchange.module.scss';
 
 interface CurrencyExchangeProps {
@@ -15,39 +16,73 @@ export default function CurrencyExchange({
   countryCode,
 }: CurrencyExchangeProps) {
   const [countryCurrency, setCountryCurrency] = useState(1);
-  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const [inputFlag, setInputFlag] = useState(
+    `https://www.countryflagicons.com/FLAT/32/AE.png`
+  );
+  const [selectedCurrency, setSelectedCurrency] = useState(
+    Object.keys(currencyCountryCodes)[0]
+  );
   const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [isCurrencyInputOpen, setIsCurrencyInputOpen] = useState(false);
 
-  const exchangeRateUrl = `${baseExchangeRateUrl}${
-    import.meta.env.VITE_EXCHANGERATE_API_KEY
-  }/latest/${currency}`;
+  const supportedCurrenciesUrl =
+    'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json';
 
-  const supportedCurrenciesUrl = `${baseExchangeRateUrl}${
-    import.meta.env.VITE_EXCHANGERATE_API_KEY
-  }/codes`;
-
-  const [currencyData] = useFetch<IExchangeRates>(exchangeRateUrl);
   const [currencyCodes] = useFetch<IExchangeRates>(supportedCurrenciesUrl);
   const [countriesData] = useFetch('https://restcountries.com/v3.1/all');
 
-  let currencyOptions;
-  if (currencyCodes) {
-    currencyOptions = currencyCodes.supported_codes?.map((currencyCode) => (
-      <li key={currencyCode[0]} className={classes.currencyOptionItem}>
-        {currencyCode[1]}
-      </li>
-    ));
+  const countryFlagImgSrc = `https://www.countryflagicons.com/FLAT/32/${countryCode}.png`;
+
+  function handleCurrencyChange(e) {
+    setIsSelectOpen(true);
+    setSelectedCurrency(e.target.value.toUpperCase());
   }
 
-  const countryFlagImgSrc = `https://flagsapi.com/${countryCode}/flat/16.png`;
-
-  function handleChange(e) {
-    setSelectedCurrency(e.target.value);
+  function handleCurrencyInput() {
+    setIsSelectOpen(true);
+    setIsCurrencyInputOpen(true);
+    console.log(isSelectOpen);
   }
 
-  function toggleDropDown() {
-    setIsSelectOpen(!isSelectOpen);
+  if (currencyCodes) console.log(currencyCodes);
+
+  function userSelectedCurrency(currency, countryCode) {
+    setIsSelectOpen(false);
+    setIsCurrencyInputOpen(false);
+    setSelectedCurrency(currency);
+    setInputFlag(`https://www.countryflagicons.com/FLAT/32/${countryCode}.png`);
   }
+
+  const currencyOptions = Object.keys(currencyCountryCodes).map((currencyCode, i) => (
+    <li
+      key={i}
+      className={classes.currencyListItem}
+      onClick={() =>
+        userSelectedCurrency(currencyCode, currencyCountryCodes[currencyCode])
+      }
+    >
+      <div className={classes.listItemDiv}>
+        {currencyCode}
+        <img
+          src={`https://www.countryflagicons.com/FLAT/32/${currencyCountryCodes[currencyCode]}.png`}
+          alt='country flag'
+        />
+      </div>
+    </li>
+  ));
+
+  function handleClickOutside() {
+    setIsSelectOpen(false);
+    console.log('clicked');
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
@@ -60,7 +95,6 @@ export default function CurrencyExchange({
             <input
               type='number'
               value={countryCurrency}
-              onChange={handleChange}
               className={classes.currencyInput}
             />
           </div>
@@ -85,19 +119,27 @@ export default function CurrencyExchange({
             />
           </div>
           <div className={classes.selectWrapper}>
-            <label htmlFor='currency' className={classes.label}>
-              Euro
-            </label>
-            <div className={classes.dropdown} onClick={toggleDropDown} tabIndex='0'>
-              countries
-              {isSelectOpen && (
-                <ul className={classes.currencyOptionsList}>{currencyOptions}</ul>
-              )}
+            <div className={classes.inputFlag}>
+              <div className={classes.selectInputWrapper} onClick={handleCurrencyInput}>
+                {!isCurrencyInputOpen && <img src={inputFlag} alt='' />}
+                {!isCurrencyInputOpen && <span>{selectedCurrency}</span>}
+                {isCurrencyInputOpen && (
+                  <input
+                    type='text'
+                    value={selectedCurrency}
+                    className={classes.dropdown}
+                    onChange={handleCurrencyChange}
+                    onClick={() => setIsSelectOpen(true)}
+                  />
+                )}
+              </div>
             </div>
+            {isSelectOpen && (
+              <ul className={classes.currencyOptionsList}>{currencyOptions}</ul>
+            )}
           </div>
         </div>
       </article>
-      {/*  <img src={countryFlag}></img> */}
     </>
   );
 }
