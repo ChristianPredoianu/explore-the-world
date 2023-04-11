806daad8214329322747d993550d79194d436731
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-11 16:21:57 +0200

    work

--- src/pages/CountryDetails.tsx
+++ src/pages/CountryDetails.tsx
@@ -153,6 +153,7 @@ export default function CountryDetails() {
           {sectionMap}
           {sectionCalendar}
           {sectionCurrencyExchange}
+          {sectionMap}
         </div>
       </>
     </>
