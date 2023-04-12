d25e87162ecc0d68367cab6e9cee586471dbe174
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-12 19:32:56 +0200

    changed variable name

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 export function useKeyPress(callback: () => void, keys: string[]) {
   function onKeyDown(event: KeyboardEvent) {
-    const wasAnyKeyPressed = keys.some((key) => event.key === key);
+    const isAnyKeyPressed = keys.some((key) => event.key === key);
 
-    if (wasAnyKeyPressed) {
+    if (isAnyKeyPressed) {
       callback();
     }
   }
