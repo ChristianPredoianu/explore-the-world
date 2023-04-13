098e023d48127fb41ef812245bca3beef7081c7a
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-13 10:38:01 +0200

    added hook

---
+++ src/hooks/useCountryNames.ts
@@ -0,0 +1,17 @@
+import { useFetch } from '@/hooks/useFetch';
+import { baseAllCountriesUrl } from '@/utils/urls';
+import { ICountryName } from '@/types/apiTypes.interface';
+
+export function useCountryNames() {
+  const [countryData] = useFetch<ICountryName[]>(baseAllCountriesUrl);
+
+  let countryNames: string[] = [];
+
+  if (countryData) {
+    countryNames = countryData.map((countryName) => countryName.name.common);
+  }
+
+  return {
+    countryNames,
+  } as const;
+}
