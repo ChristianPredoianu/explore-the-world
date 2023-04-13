006040b9d45b8df35ba714fd0205468e52677def
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-13 18:13:40 +0200

    work

--- src/components/swiper/EffectCreativeSlider.tsx
+++ src/components/swiper/EffectCreativeSlider.tsx
@@ -11,9 +11,7 @@ interface EffectCreativeSliderProps {
   images: ICountriesImages;
 }
 
-export default function EffectCreativeSlider({
-  images,
-}: EffectCreativeSliderProps) {
+export default function EffectCreativeSlider({ images }: EffectCreativeSliderProps) {
   images.photos.shift();
 
   const imgSlides = images.photos.map((image) => (
