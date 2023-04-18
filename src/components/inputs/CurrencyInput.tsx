99520cb606045ee3027b21edef892e3819027347
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-18 20:12:53 +0200

    work

--- src/components/inputs/CurrencyInput.tsx
+++ src/components/inputs/CurrencyInput.tsx
@@ -4,13 +4,30 @@ interface CurrencyInputProps {
   label?: string;
   type: string;
   value: number;
+  currencyDetails: {
+    flag: string;
+    currency: string;
+  };
 }
 
-export default function CurrencyInput({ label, type, value }: CurrencyInputProps) {
+export default function CurrencyInput({
+  label,
+  type,
+  value,
+  currencyDetails,
+}: CurrencyInputProps) {
   return (
     <div className={classes.inputWrapper}>
       <label className={classes.label}>{label}</label>
       <input type={type} value={value} className={classes.currencyInput} />
+      {currencyDetails && (
+        <div className={classes.countryCurrency}>
+          <div
+            className={`currency-flag currency-flag-${currencyDetails.flag.toLowerCase()}`}
+          ></div>
+          <p className={classes.currency}>{currencyDetails.currency}</p>
+        </div>
+      )}
     </div>
   );
 }
