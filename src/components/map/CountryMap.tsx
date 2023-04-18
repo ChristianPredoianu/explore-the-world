b84b2596bff972939f4102c70954c02f6ff2b9bc
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-18 10:11:48 +0200

    fixed map to update on new country

--- src/components/map/CountryMap.tsx
+++ src/components/map/CountryMap.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import MarkerMap from '@/components/map/MarkerMap';
 
 import { LatLngExpression } from 'leaflet';
 import '@/components/map/CountryMap.scss';
@@ -12,21 +12,21 @@ export default function CountryMap({ coords }: CountryMapProps) {
   const position: LatLngExpression = [coords[0], coords[1]]; // [latitude, longitude]
   const zoomLevel = 7;
 
-  console.log(position);
-
-  /*  useEffect(() => {
-    map.setView(coords[0], coords[1]);
-  }, [coords]); */
-
+  /*   function ChangeView({ center, zoom }) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+ */
   return (
-    <div id='map'>
+    <>
       <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
         />
-        <Marker position={position}></Marker>
+        <MarkerMap position={position} />
       </MapContainer>
-    </div>
+    </>
   );
 }
