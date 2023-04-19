305657fe3aa7fbf6a308b597a807280f812e7133
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-19 16:10:49 +0200

    removed useRef

--- src/components/currency-exchange/CurrencyExchange.tsx
+++ src/components/currency-exchange/CurrencyExchange.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import CurrencyInput from '@/components/inputs/CurrencyInput';
 import SearchInput from '@/components/inputs/SearchInput';
