56c78e7301040ddfc5c296a51d12d65fe1e1b689
Christian Predoianu <christianpredoianu@yahoo.com>
2023-04-27 16:47:52 +0200

    added bg video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,8 @@ import SlidesAutoSwiper from '@/components/swiper/SlidesAutoSwiper';
 import SearchInput from '@/components/inputs/SearchInput';
 import { baseCountryImagesUrl } from '@/utils/urls';
 import { ICountriesImages } from '@/types/apiTypes.interface';
+import backgroundVideo from '@/assets/bg-video-beach.mp4';
+import videoFallbackImg from '@/assets/images/explore.jpg';
 
 import classNames from 'classnames';
 import classes from '@/pages/Home.module.scss';
@@ -33,13 +35,13 @@ export default function Home() {
   const heroContent = (
     <div className={classNames('container', classes.heroContent)}>
       <section className={classNames(classes.heroSectionHeadings)}>
-        <p className={classes.heroParagraph}>Travel website</p>
+        <p className={classes.heroParagraph}>Adventures</p>
         <h1 className={classes.heroPrimaryHeading}>Never stop exploring the world.</h1>
         <h2 className={classes.heroSecondaryHeading}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type specimen
-          book.
+          Welcome to Wanderlust Adventures! We help you discover the world's best
+          destinations, activities, and accommodations. Our personalized service and
+          attention to detail take care of all logistics, so you can focus on making
+          unforgettable memories. Start planning your dream trip today!
         </h2>
         <div className={classes.searchInput}>
           <SearchInput
@@ -62,6 +64,16 @@ export default function Home() {
     <>
       <MainNav />
       <main className={classes.main}>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          poster={videoFallbackImg}
+          className={classes.bgVideo}
+        >
+          <source src={backgroundVideo} type='video/mp4' />
+        </video>
         <div className={classes.overlay}></div>
         {heroContent}
       </main>
