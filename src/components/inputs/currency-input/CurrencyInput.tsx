423e9e45169fa33add28b270eb4f7c8f709b3f7e
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-04 15:44:11 +0200

    new folder

---
+++ src/components/inputs/currency-input/CurrencyInput.tsx
@@ -0,0 +1,38 @@
+import classes from '@/components/inputs/currency-input/CurrencyInput.module.scss';
+
+interface CurrencyInputProps {
+  label: string;
+  value: number;
+  currencyDetails: {
+    flag: string;
+    currency: string;
+  };
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function CurrencyInput({
+  label,
+  value,
+  currencyDetails,
+  handleChange,
+}: CurrencyInputProps) {
+  return (
+    <div className={classes.inputWrapper}>
+      <label className={classes.label}>{label}</label>
+      <input
+        type='number'
+        value={value}
+        className={classes.currencyInput}
+        onChange={handleChange}
+      />
+      {currencyDetails && (
+        <div className={classes.countryCurrency}>
+          <div
+            className={`currency-flag currency-flag-${currencyDetails.flag.toLowerCase()}`}
+          ></div>
+          <p className={classes.currency}>{currencyDetails.currency}</p>
+        </div>
+      )}
+    </div>
+  );
+}
