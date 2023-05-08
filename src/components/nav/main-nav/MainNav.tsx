e0a788e2e36adac9a0795d9236f52100c1321b59
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-08 11:45:52 +0200

    added folder

---
+++ src/components/nav/main-nav/MainNav.tsx
@@ -0,0 +1,16 @@
+import SocialMediaIcons from '@/components/ui/SocialMediaIcons';
+import classNames from 'classnames';
+import classes from '@/components/nav/main-nav/MainNav.module.scss';
+
+export default function MainNav() {
+  return (
+    <>
+      <header className={classNames('container', classes.header)}>
+        <nav className={classes.nav}>
+          <p className={classes.navLogo}>Wanderlust Adventures</p>
+          <SocialMediaIcons />
+        </nav>
+      </header>
+    </>
+  );
+}
