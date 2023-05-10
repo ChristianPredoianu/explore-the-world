8d68d2f8ef418ae5c47509ccbf90f4b80d0b6f8b
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-10 14:35:52 +0200

    final

--- src/types/apiTypes.interface.ts
+++ src/types/apiTypes.interface.ts
@@ -4,7 +4,7 @@ export interface ICountriesImages {
   photos: {
     id: number;
     alt: string;
-    src: { portrait: string; small: string; large2x: string };
+    src: { portrait: string; small: string; medium: string; large2x: string };
   }[];
 }
 
