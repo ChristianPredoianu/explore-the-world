d9ea247fa56383ea3acb8f351705b281a4d83574
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-10 13:33:37 +0200

    final

--- src/components/inputs/currency-input/CurrencyInput.tsx
+++ src/components/inputs/currency-input/CurrencyInput.tsx
@@ -2,12 +2,13 @@ import classes from '@/components/inputs/currency-input/CurrencyInput.module.scs
 
 interface CurrencyInputProps {
   label: string;
-  value: number;
+  value: string;
   currencyDetails: {
     flag: string;
     currency: string;
   };
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: () => void;
 }
 
 export default function CurrencyInput({
@@ -15,15 +16,19 @@ export default function CurrencyInput({
   value,
   currencyDetails,
   handleChange,
+  handleBlur,
 }: CurrencyInputProps) {
   return (
     <div className={classes.inputWrapper}>
       <label className={classes.label}>{label}</label>
       <input
         type='number'
-        value={value}
+        value={isNaN(+value) ? '' : parseFloat(value).toFixed(4)}
+        step='0.01'
         className={classes.currencyInput}
+        disabled={label === 'You get'}
         onChange={handleChange}
+        onBlur={handleBlur}
       />
       {currencyDetails && (
         <div className={classes.countryCurrency}>
