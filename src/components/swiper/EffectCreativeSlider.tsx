6a05c23db2d41cf86f8c24a8d77b7bcc80a7989e
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-11 12:22:19 +0200

    work

--- src/components/swiper/EffectCreativeSlider.tsx
+++ src/components/swiper/EffectCreativeSlider.tsx
@@ -2,19 +2,32 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCreative } from 'swiper';
 import { ICountriesImages } from '@/types/apiTypes.interface';
 import { Autoplay } from 'swiper';
+import FlyingImg from '@/assets/images/flying.jpg';
 
 import 'swiper/css';
 import 'swiper/css/effect-creative';
 import '@/components/swiper/EffectCreativeSlider.scss';
+import { useEffect } from 'react';
 
 interface EffectCreativeSliderProps {
   images: ICountriesImages;
 }
 
 export default function EffectCreativeSlider({ images }: EffectCreativeSliderProps) {
-  images.photos.shift();
+  /*   images.photos.shift(); */
 
-  const imgSlides = images.photos.map((image) => (
+  let imgSlides;
+
+  /*   if (images.photos.length === 0) {
+    imgSlides = (
+      <SwiperSlide
+        style={{
+          backgroundImage: `url('${FlyingImg}')`,
+        }}
+      ></SwiperSlide>
+    );
+  }  */
+  imgSlides = images.photos.map((image) => (
     <SwiperSlide
       key={image.id}
       style={{
@@ -22,6 +35,17 @@ export default function EffectCreativeSlider({ images }: EffectCreativeSliderPro
       }}
     ></SwiperSlide>
   ));
+
+  /*   useEffect(() => {
+    imgSlides = images.photos.map((image) => (
+      <SwiperSlide
+        key={image.id}
+        style={{
+          backgroundImage: `url('${image.src.large2x}')`,
+        }}
+      ></SwiperSlide>
+    ));
+  }, []); */
 
   return (
     <>
