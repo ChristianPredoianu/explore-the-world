0f35f9f37e71ef5424e70656147ef64b076b590b
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-12 10:57:39 +0200

    fixed brouser error

--- src/components/inputs/search-input/SearchInput.test.tsx
+++ src/components/inputs/search-input/SearchInput.test.tsx
@@ -2,18 +2,21 @@ import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import SearchInput from './SearchInput';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('search input test', () => {
   it('should be empty', () => {
     const mockOnClickFunction = vi.fn();
     render(
-      <SearchInput
-        suggestions={['romania', 'france']}
-        placeholder={'Search country'}
-        callback={() => {
-          mockOnClickFunction;
-        }}
-      />
+      <BrowserRouter>
+        <SearchInput
+          suggestions={['romania', 'france']}
+          placeholder={'Search country'}
+          callback={() => {
+            mockOnClickFunction;
+          }}
+        />
+      </BrowserRouter>
     );
   });
 });
