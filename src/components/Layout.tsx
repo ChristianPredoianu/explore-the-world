36b942bed41cde1b388ecf02c16bbbf377c9aa7d
Christian Predoianu <christianpredoianu@yahoo.com>
2023-08-17 11:29:17 +0200

    show footer when not home page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './footer/Footer';
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
     <>
       <Outlet />
-      <Footer />
+      {location.pathname !== '/' && <Footer />}
     </>
   );
 }
