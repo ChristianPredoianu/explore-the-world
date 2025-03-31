f1bf3846444651c191def9a685e0a106ef7b4c7c
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-31 11:55:35 +0200

    deleted

--- src/components/ui/loading-spinner/LodingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import classes from '@/LoadingSpinner.module.scss';
-
-export default function LoadingSpinner() {
-  return (
-    <>
-      <div className={classes.loader}>Loading...</div>
-    </>
-  );
-}
