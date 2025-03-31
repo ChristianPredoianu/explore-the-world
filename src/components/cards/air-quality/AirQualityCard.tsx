f2186e93b37eb0435975321c35c8f3b48747d499
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-31 14:09:26 +0200

    refactored

--- src/components/cards/air-quality/AirQualityCard.tsx
+++ src/components/cards/air-quality/AirQualityCard.tsx
@@ -2,6 +2,7 @@ import { useFetch } from '@/hooks/useFetch';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { baseAirVisualUrl } from '@/utils/urls';
 import { IAirQualityData, IPollutionInfo } from '@/types/apiTypes.interface';
+import { getPolutionInfo } from '@/utils/airQuality';
 import classes from '@/components/cards/air-quality/AirQualityCard.module.scss';
 
 interface AirQualityCardProps {
@@ -16,75 +17,26 @@ export default function AirQualityCard({ coords, country }: AirQualityCardProps)
 
   const [airQualityData, error] = useFetch<IAirQualityData>(airVisualUrl);
 
-  let pollutionInfo!: IPollutionInfo;
+  let pollutionInfo: IPollutionInfo | undefined;
 
   if (airQualityData) {
-    const airQualityValue: number = airQualityData.data.current.pollution.aqius;
-
-    switch (true) {
-      case airQualityValue <= 50:
-        pollutionInfo = {
-          color: 'hsl(127, 100%, 42%)',
-          icon: ['fas', 'smile'],
-          level: 'Good',
-          desc: 'Air quality is satisfactory, and air pollution poses little or no risk.',
-        };
-        break;
-      case airQualityValue > 50 && airQualityValue <= 100:
-        pollutionInfo = {
-          color: 'hsl(46, 100%, 50%)',
-          icon: ['fas', 'meh'],
-          level: 'Moderate',
-          desc: 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.',
-        };
-        break;
-      case airQualityValue > 100 && airQualityValue <= 150:
-        pollutionInfo = {
-          color: 'hsl(41, 89%, 44%)',
-          icon: ['fas', 'sad-tear'],
-          level: 'Unhealthy for Sensitive Groups',
-          desc: 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.',
-        };
-        break;
-      case airQualityValue > 150 && airQualityValue <= 200:
-        pollutionInfo = {
-          color: 'hsl(4, 100%, 42%)',
-          icon: ['fas', 'sad-tear'],
-          level: 'Unhealthy',
-          desc: 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.',
-        };
-        break;
-      case airQualityValue > 200 && airQualityValue <= 300:
-        pollutionInfo = {
-          color: 'hsl(274, 100%, 42%)',
-          icon: ['fas', 'sad-tear'],
-          level: 'Very Unhealthy',
-          desc: 'Health alert: The risk of health effects is increased for everyone.',
-        };
-        break;
-      case airQualityValue < 300:
-        pollutionInfo = {
-          color: 'hsl(353, 62%, 56%)',
-          icon: ['fas', 'sad-tear'],
-          level: 'Hazardous',
-          desc: 'Health warning of emergency conditions: everyone is more likely to be affected.',
-        };
-        break;
-    }
+    const airQualityValue = airQualityData.data.current.pollution.aqius;
+    pollutionInfo = getPolutionInfo(airQualityValue);
   }
 
   let airQualityError;
 
-  if (error)
+  if (error) {
     airQualityError = (
       <p className={classes.error}>
-        {`There is no air quality data avaliable for ${country}`}
+        {`There is no air quality data available for ${country}`}
       </p>
     );
+  }
 
   let airQualityCard;
 
-  if (airQualityData) {
+  if (airQualityData && pollutionInfo) {
     airQualityCard = (
       <article className={classes.card}>
         <div
