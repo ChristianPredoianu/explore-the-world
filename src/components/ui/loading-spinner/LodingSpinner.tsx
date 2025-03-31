38391f50c44979258ca1ec68b8a338e5f04e45cf
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-31 11:31:43 +0200

    added loading spinner

---
+++ src/components/ui/loading-spinner/LodingSpinner.tsx
@@ -0,0 +1,9 @@
+import classes from '@/LoadingSpinner.module.scss';
+
+export default function LoadingSpinner() {
+  return (
+    <>
+      <div className={classes.loader}>Loading...</div>
+    </>
+  );
+}
