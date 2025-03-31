3f0a8d40735b9c6156399f6518244b54ffc3fc75
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-31 14:23:21 +0200

    refactored

--- src/components/cards/forecast/ForecastCard.tsx
+++ src/components/cards/forecast/ForecastCard.tsx
@@ -5,7 +5,7 @@ interface ForecastCardProps {
   data: IOpenWeatherForecast;
 }
 
-const days = [
+const WEEKDAYS = [
   'Monday',
   'Tuesday',
   'Wednesday',
@@ -18,26 +18,23 @@ const days = [
 export default function ForecastCard({ data }: ForecastCardProps) {
   function getDayOfWeek() {
     const dayNum = new Date(data.dt * 1000).getDay();
-    const dayOfWeek = days[dayNum];
-
-    return dayOfWeek;
+    return WEEKDAYS[dayNum];
   }
 
   return (
-    <>
-      <article className={classes.card}>
-        <h5 className={classes.cardHeading}>{getDayOfWeek()}</h5>
-        <img
-          src={` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-          alt='weather icon'
-        />
-        <div className={classes.temperature}>
-          <p className={classes.tempMax}>{`${data.main.temp_max.toFixed()} \xBAC`}</p>
-          <p className={classes.tempMin}>
-            {data.weather[0].main /* `${data.main.temp_min.toFixed()} \xBAC` */}
-          </p>
-        </div>
-      </article>
-    </>
+    <article className={classes.card}>
+      <h5 className={classes.cardHeading}>{getDayOfWeek()}</h5>
+      <img
+        src={` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        alt={'Weather icon'}
+        loading='lazy'
+      />
+      <div className={classes.temperature}>
+        <p className={classes.tempMax}>{`${data.main.temp_max.toFixed()} \xBAC`}</p>
+        <p className={classes.tempMin}>
+          {data.weather[0].main /* `${data.main.temp_min.toFixed()} \xBAC` */}
+        </p>
+      </div>
+    </article>
   );
 }
