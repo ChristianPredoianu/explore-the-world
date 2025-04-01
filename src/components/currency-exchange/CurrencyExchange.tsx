db94abd0aba0cd9d954b5cc220d50123b758d6ba
Christian Predoianu <christianpredoianu@yahoo.com>
2025-04-01 12:53:13 +0200

    fixed hook

--- src/components/currency-exchange/CurrencyExchange.tsx
+++ src/components/currency-exchange/CurrencyExchange.tsx
@@ -1,95 +1,27 @@
-import { useState, useEffect } from 'react';
-import { useFetch } from '@/hooks/useFetch';
+import { useCurrencyExchange } from '@/hooks/useCurrencyExchange';
 import CurrencyInput from '@/components/inputs/currency-input/CurrencyInput';
 import SearchInput from '@/components/inputs/search-input/SearchInput';
-import { currencyCountryCodes } from '@/utils/currencies';
-import { IExchangeRates } from '@/types/apiTypes.interface';
-import { baseCurrencyRatesUrl } from '@/utils/urls';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from '@/components/currency-exchange/CurrencyExchange.module.scss';
-import '@/components/inputs/CurrencyFlags.scss';
 
 interface CurrencyExchangeProps {
   currency: string;
 }
 
 export default function CurrencyExchange({ currency }: CurrencyExchangeProps) {
-  const [currencyFromValue, setCurrencyFromValue] = useState('1');
-  const [currencyToValue, setCurrencyToValue] = useState('0');
-  const [initialCurrency, setInitialCurrency] = useState(currency);
-  const [selectedCurrency, setSelectedCurrency] = useState(
-    Object.keys(currencyCountryCodes)[47]
-  );
-  const [isFlipped, setIsFlipped] = useState(false);
-  const [countryFlagFrom, setCountryFlagFrom] = useState(currency.toLowerCase());
-  const [countryFlagTo, setCountryFlagTo] = useState('eur');
-
-  const supportedCurrenciesUrl = `${baseCurrencyRatesUrl}/${currency.toLowerCase()}.json`;
-
-  const [initialCurrencyExchRates] = useFetch<IExchangeRates>(supportedCurrenciesUrl);
-
-  const currencyArray = Object.keys(currencyCountryCodes);
-
-  function getExchangeRates(userSelectedCurrency: string) {
-    if (isFlipped) {
-      setIsFlipped(false);
-      setCurrencyFromValue('1');
-    }
-
-    if (userSelectedCurrency !== undefined) {
-      setCountryFlagTo(userSelectedCurrency);
-      setSelectedCurrency(userSelectedCurrency);
-    }
-  }
-
-  function toggleIsFlipped() {
-    setIsFlipped(!isFlipped);
-  }
-
-  function initialConversion() {
-    if (initialCurrencyExchRates) {
-      setCurrencyToValue(
-        parseFloat(
-          (
-            initialCurrencyExchRates[currency.toLowerCase()][
-              selectedCurrency.toLowerCase()
-            ] * +currencyFromValue
-          ).toString()
-        ).toFixed(2)
-      );
-    }
-  }
-
-  function flipConversion() {
-    if (initialCurrencyExchRates) {
-      setCurrencyToValue(
-        parseFloat(
-          (
-            +currencyFromValue /
-            initialCurrencyExchRates[currency.toLowerCase()][
-              initialCurrency.toLowerCase()
-            ]
-          ).toString()
-        ).toFixed(2)
-      );
-    }
-  }
-
-  function flip() {
-    toggleIsFlipped();
-
-    let tempValue = currencyFromValue;
-    setCurrencyFromValue(currencyToValue);
-    setCurrencyToValue(tempValue);
-
-    let tempFlag = countryFlagTo;
-    setCountryFlagTo(countryFlagFrom);
-    setCountryFlagFrom(tempFlag);
-
-    let tempCurrency = initialCurrency;
-    setInitialCurrency(selectedCurrency.toUpperCase());
-    setSelectedCurrency(tempCurrency);
-  }
+  const {
+    currencyFromValue,
+    setCurrencyFromValue,
+    currencyToValue,
+    setCurrencyToValue,
+    initialCurrency,
+    selectedCurrency,
+    countryFlagFrom,
+    countryFlagTo,
+    currencyArray,
+    getExchangeRates,
+    flip,
+  } = useCurrencyExchange(currency);
 
   function handleCurrencyFromChange(e: React.ChangeEvent<HTMLInputElement>) {
     const newValue = parseFloat(e.target.value.replace(',', '.'));
@@ -108,71 +40,43 @@ export default function CurrencyExchange({ currency }: CurrencyExchangeProps) {
     setCurrencyFromValue(currencyToValue.toLocaleString());
   }
 
-  useEffect(() => {
-    setInitialCurrency(currency);
-    setCountryFlagFrom(currency.toLowerCase());
-  }, [currency]);
-
-  useEffect(() => {
-    if (!isFlipped) {
-      setInitialCurrency(currency);
-      setCountryFlagFrom(currency.toLowerCase());
-    }
-  }, [isFlipped]);
-
-  useEffect(() => {
-    isFlipped ? flipConversion() : initialConversion();
-  }, [currencyFromValue, selectedCurrency, initialCurrencyExchRates]);
-
-  const searchInput = (
-    <div className={classes.searchInputWrapper}>
-      <SearchInput
-        suggestions={currencyArray}
-        placeholder={'Currency'}
-        callback={getExchangeRates}
-      />
-    </div>
-  );
-
-  const currencyFrom = (
-    <div className={classes.currencyFrom}>
-      <CurrencyInput
-        label={'Amount'}
-        value={currencyFromValue}
-        handleChange={handleCurrencyFromChange}
-        handleBlur={handleCurrencyFromBlur}
-        currencyDetails={{ flag: countryFlagFrom, currency: initialCurrency }}
-      />
-    </div>
-  );
-
-  const flipIcon = (
-    <FontAwesomeIcon icon={['fas', 'repeat']} className={classes.icon} onClick={flip} />
-  );
-
-  const currencyTo = (
-    <div className={classes.currencyTo}>
-      <CurrencyInput
-        label={'You get'}
-        value={currencyToValue}
-        handleChange={handleCurrencyToChange}
-        handleBlur={handleCurrencyToBlur}
-        currencyDetails={{
-          flag: countryFlagTo.toLowerCase(),
-          currency: selectedCurrency,
-        }}
-      />
-    </div>
-  );
-
   return (
     <>
       <article className={classes.currencyCard}>
-        {searchInput}
+        <div className={classes.searchInputWrapper}>
+          <SearchInput
+            suggestions={currencyArray}
+            placeholder={'Currency'}
+            callback={getExchangeRates}
+          />
+        </div>
         <div className={classes.currencyWrapper}>
-          {currencyFrom}
-          {flipIcon}
-          {currencyTo}
+          <div className={classes.currencyFrom}>
+            <CurrencyInput
+              label={'Amount'}
+              value={currencyFromValue}
+              handleChange={handleCurrencyFromChange}
+              handleBlur={handleCurrencyFromBlur}
+              currencyDetails={{ flag: countryFlagFrom, currency: initialCurrency }}
+            />
+          </div>
+          <FontAwesomeIcon
+            icon={['fas', 'repeat']}
+            className={classes.icon}
+            onClick={flip}
+          />
+          <div className={classes.currencyTo}>
+            <CurrencyInput
+              label={'You get'}
+              value={currencyToValue}
+              handleChange={handleCurrencyToChange}
+              handleBlur={handleCurrencyToBlur}
+              currencyDetails={{
+                flag: countryFlagTo.toLowerCase(),
+                currency: selectedCurrency,
+              }}
+            />
+          </div>
         </div>
       </article>
     </>
